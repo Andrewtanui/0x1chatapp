bcac69263b34aaf11cc20d8767627f5f0874d73c
Andrewtanui <lelanandre9@gmail.com>
2024-01-25 15:01:23 +0300

    Set-up ready

--- server.py
+++ server.py
@@ -1,1 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  hello = {
+    "message":"Hello group whatever"
+  }
+  return render_template("index.html",
+                         title="Chap app project",
+                         hello=hello["message"])
+
+if __name__ == '__main__':
+  app.run(debug=True)
