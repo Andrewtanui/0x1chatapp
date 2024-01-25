00fc61cc8b2e4830468ab2561d04ea8a5411506c
Andrewtanui <lelanandre9@gmail.com>
2024-01-26 01:03:51 +0300

    Database and login setup

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, render_template
+from models import User
 
 app = Flask(__name__)
 
@@ -10,6 +11,21 @@ def home():
   return render_template("index.html",
                          title="Chap app project",
                          hello=hello["message"])
+  
+  
+@app.route('/chat')
+def chat():
+
+  return render_template("chat.html",
+                         title="Chap app project",
+                         chat=True)
+  
+  
+@app.route('/login')
+def login():
+
+  return render_template("login.html",
+                         title="Chap app project")
 
 if __name__ == '__main__':
   app.run(debug=True)
